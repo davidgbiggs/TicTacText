07248c203bfeb4f93317fad39e918ad729f8c7fc
David Graham Biggs <biggs.dg@gmail.com>
2018-09-11 17:10:05 -0400

    Have some semblance of project structure

---
+++ src/main/scala/game.scala
@@ -0,0 +1,21 @@
+class game {
+
+  var 11 = 'e'
+  var 12 = 'e'
+  var 13 = 'e'
+  var 21 = 'e'
+  var 22 = 'e'
+  var 23 = 'e'
+  var 31 = 'e'
+  var 32 = 'e'
+  var 33 = 'e'
+
+}
+
+object init {
+
+  def main(Args: Array[String]): Unit = {
+    println("Hello World")
+  }
+
+}

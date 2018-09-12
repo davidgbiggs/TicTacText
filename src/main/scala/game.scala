f5c6a013a291cdb427e2151d7810c049da67fe97
David Graham Biggs <biggs.dg@gmail.com>
2018-09-12 18:02:05 -0400

    Fixed a glitch where the player could play in an occupied space in the turn function, among other smaller fixes.

--- src/main/scala/game.scala
+++ src/main/scala/game.scala
@@ -1,53 +1,63 @@
 import scala.collection.mutable
 
-class Player(name: String, identity: Char) {
-  val title: String = name
+class Player(title: String, identity: Char) {
+  val name: String = title
   val symbol: Char = identity
 }
 
 object game {
-  var state: mutable.LinkedHashMap[String, Char] = scala.collection.mutable.LinkedHashMap[String, Char]("11" -> 'e',
-    "12" -> 'e', "13" -> 'e', "21" -> 'e', "22" -> 'e', "23" -> 'e', "31" -> 'e', "32" -> 'e', "33" -> 'e')
+  // initialize game state
+  var state: mutable.LinkedHashMap[String, Char] = scala.collection.mutable.LinkedHashMap[String, Char]("11" -> 'E',
+    "12" -> 'E', "13" -> 'E', "21" -> 'E', "22" -> 'E', "23" -> 'E', "31" -> 'E', "32" -> 'E', "33" -> 'E')
 
-  val p1 = new Player("Player 1", 'x')
-  val p2 = new Player("Player 2", 'o')
+  // initialize competitor data structures
+  val p1 = new Player("Player 1", 'X')
+  val p2 = new Player("Player 2", 'O')
 
   def didWin(): Boolean = {
     // this function returns true if a win condition is met
-    if (state("11") == state("12") && state("11") == state("13") && state("11") != 'e') true
-    else if (state("21") == state("22") && state("21") == state("23") && state("21") != 'e') true
-    else if (state("31") == state("32") && state("31") == state("33") && state("31") != 'e') true
-    else if (state("11") == state("21") && state("11") == state("31") && state("11") != 'e') true
-    else if (state("12") == state("22") && state("12") == state("32") && state("12") != 'e') true
-    else if (state("13") == state("23") && state("13") == state("33") && state("13") != 'e') true
-    else if (state("11") == state("22") && state("11") == state("33") && state("11") != 'e') true
-    else if (state("13") == state("22") && state("13") == state("31") && state("13") != 'e') true
+    if (state("11") == state("12") && state("11") == state("13") && state("11") != 'E') true
+    else if (state("21") == state("22") && state("21") == state("23") && state("21") != 'E') true
+    else if (state("31") == state("32") && state("31") == state("33") && state("31") != 'E') true
+    else if (state("11") == state("21") && state("11") == state("31") && state("11") != 'E') true
+    else if (state("12") == state("22") && state("12") == state("32") && state("12") != 'E') true
+    else if (state("13") == state("23") && state("13") == state("33") && state("13") != 'E') true
+    else if (state("11") == state("22") && state("11") == state("33") && state("11") != 'E') true
+    else if (state("13") == state("22") && state("13") == state("31") && state("13") != 'E') true
     else false
   }
 
-  def turn(player1: Player, player2: Player): Unit = {
+  def turn(turnPlayer: Player, otherPlayer: Player): Unit = {
+    // this function switches turns off until the end of the game
 
-    println("")
-    println("Where would you like to play, " + player1.title + " (" + player1.symbol + ")" + "?")
-
+    // ask the player where they want to play and receive input
+    println("\nWhere would you like to play, " + turnPlayer.name + " (" + turnPlayer.symbol + ")" + "?\n")
     var move = scala.io.StdIn.readLine()
 
+    // check if it is a valid move and loop until a legitimate move is performed
     while ((move != "11" && move != "12" && move != "13" && move != "21" && move != "22" && move != "23" && move != "31"
-      && move != "32" && move != "33") || state(move) == player2.symbol || state(move) == player1.symbol) {
+      && move != "32" && move != "33") || state(move) == otherPlayer.symbol || state(move) == turnPlayer.symbol) {
 
-      println("That space is already taken or does not exist. Try again.")
+      println("\nThat space is already taken or does not exist. Try again.\n")
       move = scala.io.StdIn.readLine()
 
     }
 
-    state(move) = player1.symbol
+    // update game state with valid move
+    state(move) = turnPlayer.symbol
 
+    // visualize updated game state
+    println()
     for ((k, v) <- state) if (k == "13" || k == "23" || k == "33") println(v + " ") else print(v + " ")
-    if (didWin()) println(player1.title + " wins!")
-    else if (state.forall{ case (_, v) => v != 'e' }) println("It's a tie!")
-    else turn(player2, player1)
+
+    // check if the latest move won, tied, or another turn is necessary
+    if (didWin()) println("\n" + turnPlayer.name + " wins!")
+    else if (state.forall { case (_, v) => v != 'E' }) println("\nIt's a tie!")
+    else turn(otherPlayer, turnPlayer)
+
   }
 
+  // start the game's conditional flow
   def main(args: Array[String]): Unit = {
     turn(p1, p2)
   }

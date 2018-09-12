97cb95434b7199e0c6b3917d58700f430ff07ea3
David Graham Biggs <biggs.dg@gmail.com>
2018-09-12 14:41:46 -0400

    Game works! Command line visual representation of the tic tac toe board.

--- src/main/scala/game.scala
+++ src/main/scala/game.scala
@@ -1,21 +1,66 @@
-class game {
+import scala.collection.mutable
 
-  var 11 = 'e'
-  var 12 = 'e'
-  var 13 = 'e'
-  var 21 = 'e'
-  var 22 = 'e'
-  var 23 = 'e'
-  var 31 = 'e'
-  var 32 = 'e'
-  var 33 = 'e'
-
+class Player(name: String, identity: Char) {
+  val title: String = name
+  val symbol: Char = identity
 }
 
-object init {
+object game {
+  var state: mutable.LinkedHashMap[String, Char] = scala.collection.mutable.LinkedHashMap[String, Char]("11" -> 'e',
+    "12" -> 'e', "13" -> 'e', "21" -> 'e', "22" -> 'e', "23" -> 'e', "31" -> 'e', "32" -> 'e', "33" -> 'e')
 
-  def main(Args: Array[String]): Unit = {
-    println("Hello World")
+  val p1 = new Player("Player 1", 'x')
+  val p2 = new Player("Player 2", 'o')
+
+  def didWin(player: Player): Boolean = {
+    if (state("11") == state("12") && state("11") == state("13") && state("11") != 'e') {
+      true
+    }
+    else if (state("21") == state("22") && state("21") == state("23") && state("21") != 'e') {
+      true
+    }
+    else if (state("31") == state("32") && state("31") == state("33") && state("31") != 'e') {
+      true
+    }
+    else if (state("11") == state("21") && state("11") == state("31") && state("11") != 'e') {
+      true
+    }
+    else if (state("12") == state("22") && state("12") == state("32") && state("12") != 'e') {
+      true
+    }
+    else if (state("13") == state("23") && state("13") == state("33") && state("13") != 'e') {
+      true
+    }
+    else if (state("11") == state("22") && state("11") == state("33") && state("11") != 'e') {
+      true
+    }
+    else if (state("13") == state("22") && state("13") == state("31") && state("13") != 'e') {
+      true
+    }
+    else false
+  }
+
+  def turn(player1: Player, player2: Player): Unit = {
+
+    println("")
+    println("Where would you like to play, " + player1.title + " (" + player1.symbol + ")" + "?")
+
+    var move = scala.io.StdIn.readLine()
+    while (state.get(move).isEmpty && move != "11" && move != "12" && move != "13" && move != "21" && move != "22" && move != "23" && move != "31" && move != "32" && move != "33") {
+      println("That space is already taken or does not exist. Try again.")
+      move = scala.io.StdIn.readLine()
+    }
+
+    state(move) = player1.symbol
+
+    for ((k, v) <- state) if (k == "13" || k == "23" || k == "33") println(v + " ") else print(v + " ")
+    if (didWin(player1)) println(player1.title + " wins!")
+    else if (state.forall{ case (_, v) => v != 'e' }) println("It's a tie!")
+    else turn(player2, player1)
+  }
+
+  def main(args: Array[String]): Unit = {
+    turn(p1, p2)
   }
 
 }
